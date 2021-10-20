49f98834fff39c644894a479e0fc11b85fcea4c4
Jason Healy <7109973+jasonhealy@users.noreply.github.com>
2021-10-20 11:49:31 +0100

    chore: install deps

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
  * @format
  */
 
+import './shim';
+import 'react-native-get-random-values';
+import '@ethersproject/shims';
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
