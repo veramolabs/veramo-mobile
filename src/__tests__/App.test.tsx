b974785f52a1acb3315519669b31c5f271a7c23c
Jason Healy <7109973+jasonhealy@users.noreply.github.com>
2021-10-28 17:28:39 +0100

    style: add husky and prettier

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import App from '../App'
+
+describe('App', () => {
+  it('does not explode', async () => {
+    const { toJSON } = render(<App />)
+
+    expect(toJSON()).toMatchSnapshot()
+  })
+})
